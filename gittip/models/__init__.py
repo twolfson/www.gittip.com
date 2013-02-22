925759c90f9797a7756b69de37b64b3dbb3623af
Chad Whitacre <chad@zetaweb.com>
2013-02-22 06:25:47 -0500

    Fix regressions in test suite; #586
    I needed an import to properly register the Deletion model with our
    SQLAlchemy wiring, so that the deletions table was emptied on teardown.

--- gittip/models/__init__.py
+++ gittip/models/__init__.py
@@ -1,4 +1,12 @@
+# We don't use these two yet, but for testing to work we need them here.
+# (If we don't import them then they aren't registered with gittip.orm.db
+# .metadata, and thus aren't cleaned up properly during teardown.)
+
 from gittip.models.absorption import Absorption
+from gittip.models.deletion import Deletion
+
+
+# The rest we actually use.
 from gittip.models.elsewhere import Elsewhere
 from gittip.models.exchange import Exchange
 from gittip.models.participant import Participant
